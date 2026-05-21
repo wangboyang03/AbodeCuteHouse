85efa3ce5410a3690a703e534b09c1a2f5606279
wangboyang <18032391370@163.com>
2026-05-22 04:13:33 +0800

    feat(network): In the main project, a dio network request utility dependency was added and further encapsulated, declaring constants such as the base address, timeout, and business logic code. The utility class exposed the methods needed to send network requests and further processed the returned data.

---
+++ lib/constants/index.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String BASE_URL = 'https://live-api.itheima.net/'; // 网络请求基地址
+  static const int NEW_WORK_TIME_OUT = 10; // 网络请求超时时间 单位秒
+  static const int SUCCESS_CODE = 10000; // 成功业务码
+}
---
+++ lib/utils/NetworkRequestUtil.dart
@@ -0,0 +1,59 @@
+import 'package:dio/dio.dart';
+import '../constants/index.dart';
+
+class NetworkRequestUtil {
+  final dio = Dio();
+  get(String url, {Map<String, dynamic>? params}) {
+    return dio.get(url, queryParameters: params);
+  }
+  post(String url, {Map<String, dynamic>? data}) {
+    return dio.post(url, data: data);
+  }
+  put(String url, {Map<String, dynamic>? data}) {
+    return dio.put(url, data: data);
+  }
+  delete(String url, {Map<String, dynamic>? data, Map<String, dynamic>? params}) {
+    return dio.delete(url, data: data, queryParameters: params);
+  }
+  upload(String url) {}
+
+  // 处理返回数据
+  dynamic handleResponse(Future<Response<dynamic>> task) async {
+    final result = await task;
+    // 通过业务码判断
+    if (result.data['code'] == Constants.SUCCESS_CODE) {
+      return result.data['data']; // fullfiled
+    }
+    // 响应失败了
+    throw DioException(requestOptions: result.requestOptions); // rejected
+  }
+
+  NetworkRequest() {
+    dio.options.baseUrl = Constants.BASE_URL;
+    dio.options.connectTimeout = const Duration(seconds: Constants.NEW_WORK_TIME_OUT);
+    dio.options.receiveTimeout = const Duration(seconds: Constants.NEW_WORK_TIME_OUT);
+    dio.options.sendTimeout = const Duration(seconds: Constants.NEW_WORK_TIME_OUT);
+
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (context, handler) {
+        // 请求拦截器
+        // 注入token...
+        handler.next(context);
+      },onResponse: (context, handler) {
+        // 需要判断Http状态码 只有2XX才认为是成功请求
+        if (context.statusCode! >= 200 && context.statusCode! <300) {
+          // 响应拦截器
+          handler.next(context);
+        } else {
+          // 响应失败了
+          handler.reject(DioException(requestOptions: context.requestOptions));
+        }
+      },onError: (context, handler) {
+        // 错误拦截器
+        handler.reject(context);
+      }
+    ));
+  }
+}
+
+final networkRequestUtil = NetworkRequestUtil();
