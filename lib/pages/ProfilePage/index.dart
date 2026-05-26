b2b293e68026d0dfe80a994f31a53369d53d9282
wangboyang <18032391370@163.com>
2026-05-26 09:54:47 +0800

    feat(profile): Added a dependency library to support image selection on HarmonyOS, implementing the logic for selecting photos by clicking on the album and improving the logic for retrieving the user's avatar. The browser and mobile interfaces are differentiated by determining whether the interface is a web application.

--- lib/pages/ProfilePage/index.dart
+++ lib/pages/ProfilePage/index.dart
@@ -1,23 +1,31 @@
+import 'dart:io';
+
 import 'package:abode_cute_house/api/user.dart';
 import 'package:abode_cute_house/controller/UserSharedController.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../utils/CustomToastUtil.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
-  @override State<ProfilePage> createState()=> ProfilePageState();
+  @override
+  State<ProfilePage> createState() => ProfilePageState();
 }
 
 class ProfilePageState extends State<ProfilePage> {
   UserSharedcontroller controller = Get.find();
   TextEditingController nickController = TextEditingController();
+  String tempAvatar = ""; // 临时存储头像
 
-  @override void initState() {
+  @override
+  void initState() {
     super.initState();
     nickController.text = controller.userInformation["nickName"] ?? "";
   }
+
   // 保存用户信息
   saveUserInformation() async {
     if (nickController.text.isEmpty) {
@@ -31,7 +39,8 @@ class ProfilePageState extends State<ProfilePage> {
     await updateUserInformationApi({"nickName": nickController.text});
     PromptAction.showSuccess("用户资料保存成功");
     controller.userInformation["nickName"] = nickController.text; // 更新对象里面的属性
-    controller.updateUserInformation(controller.userInformation); // 更新用户共享状态 响应式更新
+    controller
+        .updateUserInformation(controller.userInformation); // 更新用户共享状态 响应式更新
   }
 
   showSelectDialog() {
@@ -54,128 +63,141 @@ class ProfilePageState extends State<ProfilePage> {
   }
 
   Widget getUserAvatar() {
-    if (controller.userInformation["avatar"] != "" && controller.userInformation["avatar"] != null) {
-      return Image.network(controller.userInformation["avatar"], width: 30, height: 30);
+    if (!tempAvatar.isEmpty) {
+      if (kIsWeb) {
+        return Image.network(tempAvatar, width: 30, height: 30);
+      }
+      return Image.file(File(tempAvatar), width: 30, height: 30);
+    }
+    if (controller.userInformation["avatar"] != "" &&
+        controller.userInformation["avatar"] != null) {
+      return Image.network(controller.userInformation["avatar"],
+          width: 30, height: 30);
     }
     return Image.asset('assets/images/avatar_1.jpg', width: 30, height: 30);
+  }
+
+  openLibrary() async {
+    ImagePicker picker = ImagePicker();
+    XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      // PromptAction.showSuccess("选择照片成功");
+      tempAvatar = file.path;
+      setState(() {});
+      Navigator.pop(context);
+    }
   }
 
   // 点击头像弹出选择头像模态层
   Widget getAvatarFromCamera() {
     return SizedBox(
-      height: 50,
-      child: GestureDetector(
-        child: Container(
-          decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 0.5))),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Icon(Icons.camera_alt), SizedBox(width: 10), Text("拍照")]
-          )
-        ),
-        onTap: () {}
-      )
-    );
+        height: 50,
+        child: GestureDetector(
+            child: Container(
+                decoration: const BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.grey, width: 0.5))),
+                child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.camera_alt),
+                      SizedBox(width: 10),
+                      Text("拍照")
+                    ])),
+            onTap: () {}));
   }
 
   Widget getAvatarFromPhotos() {
     return SizedBox(
       height: 50,
       child: GestureDetector(
-        child: Container(
-          decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 0.5))),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Icon(Icons.photo_library), SizedBox(width: 10), Text("相册")]
-          )
-        ),
-        onTap: () {}
-      ),
+          child: Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(color: Colors.grey, width: 0.5))),
+              child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.photo_library),
+                    SizedBox(width: 10),
+                    Text("相册")
+                  ])),
+          onTap: () {
+            openLibrary();
+          }),
     );
   }
 
   Widget getCancel() {
     return GestureDetector(
-      child: const SizedBox(
-        height: 50,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Icon(Icons.cancel), SizedBox(width: 10), Text("取消")]
-        )
-      ),
-      onTap: () {
-        Navigator.pop(context);
-      }
-    );
+        child: const SizedBox(
+            height: 50,
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Icon(Icons.cancel),
+              SizedBox(width: 10),
+              Text("取消")
+            ])),
+        onTap: () {
+          Navigator.pop(context);
+        });
   }
 
-  @override Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('个人中心'),
-        backgroundColor: Colors.transparent,
-        centerTitle: true
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        children: [
+        appBar: AppBar(
+            title: const Text('个人中心'),
+            backgroundColor: Colors.transparent,
+            centerTitle: true),
+        backgroundColor: Colors.white,
+        body: ListView(padding: EdgeInsets.all(10), children: [
           GestureDetector(
             onTap: () {
               showSelectDialog();
             },
             child: SizedBox(
-              height: 40,
-              child: Row(
-                children: [
+                height: 40,
+                child: Row(children: [
                   const Text('头像', style: TextStyle(fontSize: 16)),
                   const Spacer(),
-                  Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
-                        // child: Image.asset('assets/images/avatar.png', width: 30, height: 30)
-                        child: getUserAvatar(),
-                      ),
-                      const Icon(Icons.arrow_back_ios, size: 12)
-                    ]
-                  )
-                ]
-              )
-            ),
+                  Row(children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      // child: Image.asset('assets/images/avatar.png', width: 30, height: 30)
+                      child: getUserAvatar(),
+                    ),
+                    const Icon(Icons.arrow_back_ios, size: 12)
+                  ])
+                ])),
           ),
-          Row(
-            children: [
-              const Text('昵称'),
-              const Spacer(),
-              Expanded(
-                child: TextField(
-                  controller: nickController,
-                  decoration: const InputDecoration(hintText: '请输入昵称', border: InputBorder.none),
-                  textAlign: TextAlign.right,
-                ),
+          Row(children: [
+            const Text('昵称'),
+            const Spacer(),
+            Expanded(
+              child: TextField(
+                controller: nickController,
+                decoration: const InputDecoration(
+                    hintText: '请输入昵称', border: InputBorder.none),
+                textAlign: TextAlign.right,
               ),
-              const Icon(Icons.arrow_forward_ios, size: 12)
-            ]
-          ),
+            ),
+            const Icon(Icons.arrow_forward_ios, size: 12)
+          ]),
           const SizedBox(height: 20),
-          Row(
-            children: [
-              Expanded(
+          Row(children: [
+            Expanded(
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 85, 145, 175),
-                    minimumSize: const Size(100, 50),
-                  ),
-                  onPressed: () {  
-                    saveUserInformation();
-                  },
-                  child: const Text('保存', style: TextStyle(color: Colors.white, fontSize: 20)),
-                )
-              )
-            ]
-          )
-        ]
-      )
-    );
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 85, 145, 175),
+                minimumSize: const Size(100, 50),
+              ),
+              onPressed: () {
+                saveUserInformation();
+              },
+              child: const Text('保存',
+                  style: TextStyle(color: Colors.white, fontSize: 20)),
+            ))
+          ])
+        ]));
   }
 }
