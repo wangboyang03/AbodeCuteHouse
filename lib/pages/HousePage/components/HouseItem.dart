1ace1886fe6b9d9914fcad70c0d1f7e16f81c0d4
wangboyang <18032391370@163.com>
2026-05-29 09:25:28 +0800

    feat(houseItem): Update the status labels for housing items to dynamic text and color schemes. Add two utility methods, getStatusText and getStatusColor, to map status codes to corresponding Chinese status text and color schemes, replacing the original hard-coded status display content and adapting to various review statuses.

--- lib/pages/HousePage/components/HouseItem.dart
+++ lib/pages/HousePage/components/HouseItem.dart
@@ -8,6 +8,43 @@ class HouseItem extends StatefulWidget {
 }
 
 class HouseItemState extends State<HouseItem> {
+  // 返回枚举房屋状态
+  String getStatusText(int val) {
+    switch (val) {
+      case 1:
+        return "审核中";
+      case 2:
+        return "审核成功";
+      case 3:
+        return "审核失败";
+      default:
+    }
+    return "未知状态";
+  }
+
+  // 返回背景色和前景色
+  List<Color> getStatusColor(int val) {
+    switch (val) {
+      case 1:
+        return [
+          const Color.fromARGB(255, 150, 198, 237),
+          const Color.fromARGB(255, 5, 130, 233)
+        ];
+      case 2:
+        return [
+          const Color.fromARGB(255, 190, 240, 186),
+          const Color.fromARGB(255, 42, 239, 11)
+        ];
+      case 3:
+        return [
+          const Color.fromARGB(255, 239, 223, 195),
+          const Color.fromARGB(255, 239, 145, 5)
+        ];
+      default:
+    }
+    return [];
+  }
+
   @override Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
@@ -19,8 +56,8 @@ class HouseItemState extends State<HouseItem> {
               const Spacer(),
               Container(
                 padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
-                decoration: BoxDecoration(color: const Color.fromARGB(50, 91, 177, 227), borderRadius: BorderRadius.circular(5)),
-                child: const Text('已绑定', style: TextStyle(color: Color.fromARGB(255, 85, 145, 175)))
+                decoration: BoxDecoration(color: getStatusColor(widget.data["status"])[0], borderRadius: BorderRadius.circular(5)),
+                child: Text(getStatusText(widget.data["status"]), style: TextStyle(color: getStatusColor(widget.data["status"])[1]))
               )
             ],
           ),
